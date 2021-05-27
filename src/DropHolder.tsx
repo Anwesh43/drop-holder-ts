41be39e699b41f68d249ae6b194a6d62f62b657c
Anwesh43 <anweshthecool0@gmail.com>
2021-05-27 14:35:29 +0530

    adding DropHolder component

---
+++ src/DropHolder.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {useStyle} from './hooks'
+import withContainer from './withContainer'
+
+interface DropHolderProps {
+    w : number, 
+    h : number,
+    scale : number, 
+    onClick : Function, 
+    items : Array<string>
+}
+const DropHolder : React.FC<DropHolderProps> = (props : DropHolderProps) => {
+    const {dropBarStyle, itemStyle} = useStyle(props.w, props.h, props.scale)
+    return (
+        <React.Fragment>
+            <div style = {dropBarStyle(props.items.length)}>
+                {props.items.map((item, i) => (
+                    <span style = {itemStyle()}>{item}</span>
+                ))}
+            </div>
+            <button onClick = {props.onClick()} style = {{left : `${props.w /2}px`, position: 'absolute', top : `${props.h / 2}px`}}>
+                Open
+            </button>
+        </React.Fragment>
+    )
+}
+
+export default withContainer(DropHolder)
