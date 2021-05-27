db8bdda86afde57f8fdf9075d5f79b4c0f48d642
Anwesh43 <anweshthecool0@gmail.com>
2021-05-27 08:44:09 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,9 +1,9 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState, CSSProperties} from 'react'
 
 const delay : number = 20 
 const scGap : number = 0.02 
 
-const useAnimatedScale = () => {
+export const useAnimatedScale = () => {
     const [scale, setScale] = useState(0)
     const [animated, setAnimated] = useState(false)
     return {
@@ -20,13 +20,13 @@ const useAnimatedScale = () => {
                         }
                         return prev + scGap 
                     })
-                })
+                }, delay)
             }
         }
     }
 }
 
-const useDimension = () => {
+export const useDimension = () => {
     const [w, setW] = useState(window.innerWidth)
     const [h, setH] = useState(window.innerHeight)
     useEffect(() => {
@@ -38,5 +38,53 @@ const useDimension = () => {
     return {
         w, 
         h
+    }
+}
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const position = 'absolute'
+    const size : number = w / 10 
+    const hSize : number = h / 15
+    const sf : number = Math.sin(scale * Math.PI)
+    return {
+        dropBarStyle(n : number) : CSSProperties {
+            const width = `${size}px`
+            const height = `${hSize * n}px`
+            const top = `${hSize * n * (sf - 1)}px`
+            const display = 'flex'
+            const flexDirection = 'column'
+            const background = 'indigo'
+            const fontSize = `${hSize / 2}px`
+            const color = 'white'
+            return {
+                position, 
+                width, 
+                height, 
+                display, 
+                flexDirection,
+                background,
+                top,
+                fontSize, 
+                color 
+            }
+        },
+        itemStyle() {
+            const borderBottom = '10px solid white'
+            const border = 'none'
+            const width = `${size}px`
+            const height = `${hSize}px`
+            const display = 'flex'
+            const justifyContent = 'center'
+            const alignItems = 'center'
+            return {
+                border, 
+                width, 
+                height,
+                borderBottom,
+                display,
+                justifyContent, 
+                alignItems
+            }
+        }
     }
 }
